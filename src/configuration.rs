10ad9aef4a137e70e7ad33d83c230cbeb310f02f
Mark Nichols <mark@zanshin.net>
2022-11-27 21:12:04 -0600

    Added connection to Postgres

--- src/configuration.rs
+++ src/configuration.rs
@@ -27,3 +27,9 @@ pub fn get_configuration() -> Result<Settings, config::ConfigError> {
     // Try to convert configuration values read into the Settings type
     settings.try_deserialize::<Settings>()
 }
+
+impl DatabaseSettings {
+    pub fn connection_string(&self) -> String {
+        format!("postgres://{}:{}@{}:{}/{}", self.username, self.password, self.host, self.port, self.database_name)
+    }
+}
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,6 +1,8 @@
 //! tests/health_check.rs
 
+use sqlx::{PgConnection, Connection};
 use std::net::TcpListener;
+use zero2prod::configuration::get_configuration;
 use zero2prod::startup::run;
 
 // Spin up an instance of our application
@@ -38,6 +40,12 @@ async fn health_check_works() {
 async fn subscribe_returns_a_200_for_valid_form_data() {
     // Arrange
     let app_address = spawn_app();
+    let configuration = get_configuration().expect("Failed to read configuration");
+    let connection_string = configuration.database.connection_string();
+    let connection = PgConnection::connect(&connection_string)
+        .await
+        .expect("Failed to connect to Postgres.");
+
     let client = reqwest::Client::new();
 
     // Act
